70456a6c613a7b14a2f912e82ccd86535a133a32
Ortega023 <50720368+Ortega023@users.noreply.github.com>
2023-06-21 18:38:04 -0500

    Update holamundo.test.js

--- __tests__/holamundo.test.js
+++ __tests__/holamundo.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../holamundo');
 describe('GET /', () => {
     it('responde con Hola Mundo!', done => {
         request(app)
